2aade11247614107db0fca2fca26323af5218570
Laurell Seville <laurellseville2000@gmail.com>
2020-11-02 19:03:36 -0500

    recommendations working

--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { recommendationsValidation } = require("../validation");
 const { API_KEY } = require("../constants");
 const axios = require("axios");
+const dynamicSort = require("../dynamicSort");
 
 router.get("/recommendations", async (req, res) => {
   try {
@@ -9,17 +10,42 @@ router.get("/recommendations", async (req, res) => {
     if (error) return res.status(400).send(error);
     let movieIds = [];
     let errors = [];
+    let recommendedMovies = [];
 
     // Get Id for each movie
     await getIdsForMoviesGivenArray(req, movieIds, errors);
 
-    console.log(movieIds)
+    // Get Recommendations for each movie using Movie Id
+    for await (id of movieIds) {
 
-    if (errors.length != 0 ) {
-        return res.status(500).send({ message: "Multiple errors have occurred: " + errors.length });
+      // get list of recommendations
+      let movieRecommendationsUrl = `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${API_KEY}&language=en-US&page=1`;
+
+      let response = await axios.get(movieRecommendationsUrl);
+      
+      let results = response.data.results;
+
+      // sort list in order of vote_average field
+      // Data is sorted in ascending order
+      results.sort(dynamicSort("vote_average"));
+
+      // add top two to recommended movies
+      // the bottom two are the highest rated
+      if (results[results.length - 1] != null ) recommendedMovies.push(results[results.length - 1])
+      if (results[results.length - 2] != null ) recommendedMovies.push(results[results.length - 2])
+
+      //console.log(results.length)
     }
 
-    return res.send({ message: "Success" });
+    //console.log(recommendedMovies)
+
+    if (errors.length != 0) {
+      return res
+        .status(500)
+        .send({ message: "Multiple errors have occurred: " + errors.length });
+    }
+
+    return res.send({ recommendedMovies });
   } catch (e) {
     console.log(e);
     return res.status(500).send({ message: "An error has occurred" });
@@ -28,23 +54,21 @@ router.get("/recommendations", async (req, res) => {
 
 module.exports = router;
 async function getIdsForMoviesGivenArray(req, movieIds, errors) {
-    for await (title of req.body.movieTitles) {
-        const movieSearchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${title}`;
-        //console.log(movieSearchUrl);
+  for await (title of req.body.movieTitles) {
+    const movieSearchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${title}`;
+    //console.log(movieSearchUrl);
 
-        await axios
-            .get(movieSearchUrl)
-            .then(function (response) {
-                //console.log(response.data.results[0].original_title);
-                movieIds.push(response.data.results[0].id);
-            })
-            .catch(function (error) {
-                errors.push("An error has occurred");
-                console.log(error);
-                return;
-            })
-            .then(function () {
-            });
-    }
+    await axios
+      .get(movieSearchUrl)
+      .then(function (response) {
+        //console.log(response.data.results[0].original_title);
+        movieIds.push(response.data.results[0].id);
+      })
+      .catch(function (error) {
+        errors.push("An error has occurred");
+        console.log(error);
+        return;
+      })
+      .then(function () {});
+  }
 }
-
